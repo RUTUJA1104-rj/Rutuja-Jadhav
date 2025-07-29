3732ccdf5d1ca40996051e4f2d549818cae83943
Rutuja_Jadhav_FBS_work <rutujajadhav280296@gmail.com>
2025-07-29 22:36:12 +0530

    Add files via upload

---
+++ Assignment No 0/q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int year;
+	if(year%4==0&&year%100!=0)
+	{
+		printf("leap year");
+		
+	}
+	else
+	{
+	}
+
+}
---
+++ Assignment No 0/q5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=15,c=7,d=12,e=15,sum,avr;
+	sum=10+15+7+12+15;
+	avr=sum/5;
+	printf("Avrege %d",avr);
+}
