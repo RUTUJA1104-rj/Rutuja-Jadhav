602bdcaa29c4217d7108bdcca6c393686e028f38
Rutuja_Jadhav_FBS_work <rutujajadhav280296@gmail.com>
2025-08-18 21:38:15 +0530

    Add files via upload

---
+++ ASSIGNMENT NO=8/Q1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[10];
+    int max, min;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = min = arr[0];  
+ // assume first element is both max and min
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    
+}
