b7c3e0a41dc7db697fbff4b6a565d299793e733d
Rutuja_Jadhav_FBS_work <rutujajadhav280296@gmail.com>
2025-12-24 00:30:30 +0530

    Delete ASSIGNMENT NO 9 directory

--- ASSIGNMENT NO 9/Reverse str.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main()
-{
-	char str[30]="rutujajadhav";
-	printf("Original %s\n",str);
-	printf("Reversed %s\n",strrev(str));
-}
--- ASSIGNMENT NO 9/lwruprcase.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main()
-{
-	char str1[]="FIRST";
-	char str2[]="bit";
-	printf("Lowercase %s\n",strlwr(str1));
-	printf("uppercase %s\n",strupr(str2));
-}
