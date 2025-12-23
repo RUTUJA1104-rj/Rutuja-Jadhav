fdc0d7c1707f6c07e5a8f84a46110e3b1a1ce623
Rutuja_Jadhav_FBS_work <rutujajadhav280296@gmail.com>
2025-12-23 22:19:42 +0530

    Delete Assignment No 0 directory

--- Assignment No 0/q1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b;
-	a=10;
-	b=15;
-	printf("addition of two number%d",a+b);
-}
--- Assignment No 0/q6.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int n=20;
-	printf("square of %d",n*n);
-	printf("cube of %d",n*n*n);
-}
